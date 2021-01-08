b62d37678f95b005eec4ade55639922f25425005
ttamsmas <ttamsmas@gmail.com>
2021-01-08 15:15:09 -0800

    Finished Collapse Logic for Nav Bar

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,34 @@
+const tech = function() {
+  $("#tech").collapse("hide")
+}
+const resume = function() {
+  $("#resume").collapse("hide")
+}
+const about = function() {
+  $("#about").collapse("hide")
+}
+const projects = function() {
+  $("#projects").collapse("hide")
+}
 $(() => {
-
-})
+  $("#buttonAbout").click(function() {
+    resume()
+    projects()
+    tech()
+  })
+  $("#buttonTech").click(function() {
+    resume()
+    about()
+    projects()
+  })
+  $("#buttonProjects").click(function() {
+    resume()
+    about()
+    tech()
+  })
+  $("#buttonResume").click(function() {
+    projects()
+    about()
+    tech()
+  })
+  })
